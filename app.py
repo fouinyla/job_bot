9af0ba611ee71869dae9e4ddc68ad37e3e8eb479
his-slowlife <67464148+his-slowlife@users.noreply.github.com>
2020-07-06 20:20:10 +0300

    CommandHandler added

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("saymehi", saymehi))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
