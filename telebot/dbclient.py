dcf961e0451c756e7dffbc90aa9cf29d390109bf
his-slowlife <67464148+his-slowlife@users.noreply.github.com>
2020-07-21 14:39:23 +0300

    singletone

--- telebot/dbclient.py
+++ telebot/dbclient.py
@@ -1,10 +1,24 @@
 import pymongo
+import sqlite3
 
 
 class DBClient:
+    __instance = None
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if DBClient.__instance == None:
+           DBClient()
+        return DBClient.__instance
+
     def __init__(self, mongodb_url):
         client = pymongo.MongoClient(mongodb_url)
         self.db = client.get_database('job_bot')
+        """ Virtually private constructor. """
+        if DBClient.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            DBClient.__instance = self
 
     def find_user(self, id):
         users = self.db.users
@@ -21,3 +35,12 @@ class DBClient:
             "firstName": first_name,
             "lastName": last_name
         })
+
+s = DBClient()
+print(s)
+
+s = DBClient.getInstance()
+print(s)
+
+s = DBClient.getInstance()
+print(s)
